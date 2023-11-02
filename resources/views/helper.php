5f7950a4d3140e3660c39677ac315b70cc3f6516
Nguyen The Tien <tiennt@hblab.vn>
2023-11-02 10:16:03 +0700

    Update helper.php

--- resources/views/helper.php
+++ resources/views/helper.php
@@ -25,13 +25,13 @@
     <?php if ($namespace == '\Illuminate\Database\Eloquent') :
         continue;
     endif; ?>
-namespace <?= $namespace == '__root' ? '' : trim($namespace, '\\') ?> { 
+namespace <?= $namespace == '__root' ? '' : trim($namespace, '\\') ?> {
     <?php foreach ($aliases as $alias) : ?>
-        <?= trim($alias->getDocComment('    ')) ?> 
+        <?= trim($alias->getDocComment('    ')) ?>
         <?= $alias->getClassType() ?> <?= $alias->getExtendsClass() ?> {
         <?php foreach ($alias->getMethods() as $method) : ?>
-            <?= trim($method->getDocComment('        ')) ?> 
-        public static function <?= $method->getName() ?>(<?= $method->getParamsWithDefault() ?>)
+            <?= trim($method->getDocComment('        ')) ?>
+        public<?= $method->isInstanceCall() ? ' static ' : ' ' ?>function <?= $method->getName() ?>(<?= $method->getParamsWithDefault() ?>)
         {<?php if ($method->getDeclaringClass() !== $method->getRoot()) : ?>
             //Method inherited from <?= $method->getDeclaringClass() ?>
          <?php endif; ?>
@@ -41,39 +41,41 @@ namespace <?= $namespace == '__root' ? '' : trim($namespace, '\\') ?> {
             <?php endif?>
             <?= $method->shouldReturn() ? 'return ' : '' ?><?= $method->getRootMethodCall() ?>;
         }
-        <?php endforeach; ?> 
+        <?php endforeach; ?>
     }
-    <?php endforeach; ?> 
+    <?php endforeach; ?>
 }
 
 <?php endforeach; ?>
 
 <?php foreach ($namespaces_by_alias_ns as $namespace => $aliases) : ?>
-namespace <?= $namespace == '__root' ? '' : trim($namespace, '\\') ?> { 
+namespace <?= $namespace == '__root' ? '' : trim($namespace, '\\') ?> {
     <?php foreach ($aliases as $alias) : ?>
         <?= $alias->getClassType() ?> <?= $alias->getShortName() ?> extends <?= $alias->getExtends() ?> {<?php if ($alias->getExtendsNamespace() == '\Illuminate\Database\Eloquent') : ?>
-            <?php foreach ($alias->getMethods() as $method) : ?> 
-                <?= trim($method->getDocComment('            ')) ?> 
-            public static function <?= $method->getName() ?>(<?= $method->getParamsWithDefault() ?>)
+            <?php foreach ($alias->getMethods() as $method) : ?>
+
+            <?= trim($method->getDocComment('            ')) ?>
+
+            public<?= $method->isInstanceCall() ? ' static ' : ' ' ?>function <?= $method->getName() ?>(<?= $method->getParamsWithDefault() ?>)
             {<?php if ($method->getDeclaringClass() !== $method->getRoot()) : ?>
                 //Method inherited from <?= $method->getDeclaringClass() ?>
-             <?php endif; ?>
+            <?php endif; ?>
 
-                <?php if ($method->isInstanceCall()) : ?>
+<?php if ($method->isInstanceCall()) : ?>
                 /** @var <?=$method->getRoot()?> $instance */
-                <?php endif?>
+<?php endif?>
                 <?= $method->shouldReturn() ? 'return ' : '' ?><?= $method->getRootMethodCall() ?>;
             }
             <?php endforeach; ?>
         <?php endif; ?>}
-    <?php endforeach; ?> 
+    <?php endforeach; ?>
 }
 
 <?php endforeach; ?>
 
 <?php if ($helpers) : ?>
 namespace {
-    <?= $helpers ?> 
+    <?= $helpers ?>
 }
 <?php endif; ?>
 
